e3bfc1b116b9a4793b75ab1728b5e1b7a583d267
Matt Rochford <mattjohnrochford@gmail.com>
2019-03-06 23:48:36 -0800

    autoformatting in vim

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+#!/bin/sh
 
 if [[ -z ~/.config ]]; then mkdir ~/.config
 fi
@@ -11,20 +10,20 @@ if [[ -z ~/.mutt ]]; then mkdir ~/.mutt
 fi
 
 rm -rf ~/.config/ranger ~/.config/rtv
-ln -sf -n $HOME/dotfiles/config/* ~/.config
-
 rm -rf ~/.mutt/accounts
-ln -sf -n $HOME/dotfiles/mutt/* ~/.mutt
 
-ln -sf $HOME/dotfiles/bash_profile ~/.bash_profile
-ln -sf $HOME/dotfiles/bashrc ~/.bashrc
-ln -sf $HOME/dotfiles/gitconfig ~/.gitconfig
-ln -sf $HOME/dotfiles/gitignore ~/.gitignore
-ln -sf $HOME/dotfiles/mailcap ~/.mailcap
-ln -sf $HOME/dotfiles/profile ~/.profile
-ln -sf $HOME/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf $HOME/dotfiles/tmux.conf.local ~/.tmux.conf.local
-ln -sf $HOME/dotfiles/vimrc ~/.vimrc
-ln -sf $HOME/dotfiles/init.vim ~/.config/nvim/init.vim
-ln -sf $HOME/dotfiles/zshrc ~/.zshrc
-ln -sf $HOME/dotfiles/ctags ~/.ctags
+ln -sf -n $PWD/src/config/* ~/.config
+ln -sf -n $PWD/src/mutt/* ~/.mutt
+
+ln -sf $PWD/src/bash_profile ~/.bash_profile
+ln -sf $PWD/src/bashrc ~/.bashrc
+ln -sf $PWD/src/gitconfig ~/.gitconfig
+ln -sf $PWD/src/gitignore ~/.gitignore
+ln -sf $PWD/src/mailcap ~/.mailcap
+ln -sf $PWD/src/profile ~/.profile
+ln -sf $PWD/src/tmux.conf ~/.tmux.conf
+ln -sf $PWD/src/tmux.conf.local ~/.tmux.conf.local
+ln -sf $PWD/src/vimrc ~/.vimrc
+ln -sf $PWD/src/init.vim ~/.config/nvim/init.vim
+ln -sf $PWD/src/zshrc ~/.zshrc
+ln -sf $PWD/src/ctags ~/.ctags
