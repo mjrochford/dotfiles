9e4e4601eff924faa753d6205fa0cd888d52e177
Matt Rochford <mattjohnrochford@gmail.com>
2019-03-14 17:30:50 -0700

    rm offlineimap

--- src/offlineimap/offlineimap_scripts.py
+++
@@ -1,8 +0,0 @@
-#!/user/bin/env python2
-from subprocess import check_output
-
-def decrypt(path):
-    return check_output("gpg -dq " + path, shell=True).strip("\n")
-
-def pass_show(identifier):
-    return check_output("pass show " + identifier, shell=True).strip("\n")
