08c1d879e7fd0b87c9237591a286872383989832
Matt Rochford <mattjohnrochford@gmail.com>
2019-11-14 01:43:43 -0800

    install bash configs

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ ln -sf $PWD/src/gitconfig ~/.gitconfig
 ln -sf $PWD/src/mailcap ~/.mailcap
 ln -sf $PWD/src/profile ~/.profile
 ln -sf $PWD/src/ctags ~/.ctags
+ln -sf $PWD/src/bashrc ~/.bashrc
+ln -sf $PWD/src/bash_profile ~/.bash_profile
 
 ./vim-plugged.sh
 
