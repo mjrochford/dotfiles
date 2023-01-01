6b1886546f0fc146e14346e0fc8bc68df02ea989
Matt Rochford <mattjohnrochford@gmail.com>
2023-01-01 15:06:43 -0800

    add a comment with packer install cmd

--- install.sh
+++ install.sh
@@ -14,4 +14,7 @@ ln -sf -n $PWD/src/config/* ~/.config
 
 exec ./vim-plugged.sh
 
+# install packer
+# git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 unset linkDotfile
